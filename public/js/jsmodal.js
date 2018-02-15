3f011e18f8ee50d386cb393ac986af9a709e7211
will <will_dex@hotmail.com>
2018-02-15 20:11:23 +0000

    laa

---
+++ public/js/jsmodal.js
@@ -0,0 +1,75 @@
+
+$('#ModalAdjudicar').on('show.bs.modal', function (event) {
+
+      var button = $(event.relatedTarget) // Botón que activó el modal
+      var idp = button.data('id') // Extraer la información de atributos de datos
+      var titulo = button.data('t') // Extraer la información de atributos de datos
+      var categoria = button.data('c') // Extraer la información de atributos de datos
+      
+      var modal = $(this)
+      modal.find('.modal-body #txtidpublic').val(idp)
+      modal.find('.modal-body #txttitulo2').val(titulo)
+      modal.find('.modal-body #txtcat2').val(categoria)
+      modal.find('.modal-body #txttitulo').val(titulo)
+      modal.find('.modal-body #txtcat').val(categoria)
+
+      $('.alert').hide();//Oculto alert
+
+
+})
+
+
+$('#ModalAdjudicarP').on('show.bs.modal', function (event) {
+
+      var button = $(event.relatedTarget) // Botón que activó el modal
+      var idp = button.data('id') // Extraer la información de atributos de datos
+      var titulo = button.data('t') // Extraer la información de atributos de datos
+      var categoria = button.data('c') // Extraer la información de atributos de datos
+      
+      var modal = $(this)
+      modal.find('.modal-body #txtidpublic').val(idp)
+      modal.find('.modal-body #txttitulo2').val(titulo)
+      modal.find('.modal-body #txtcat2').val(categoria)
+      modal.find('.modal-body #txttitulo').val(titulo)
+      modal.find('.modal-body #txtcat').val(categoria)
+
+      $('.alert').hide();//Oculto alert
+
+
+})
+
+
+$('#ModalAdjudicarC').on('show.bs.modal', function (event) {
+
+      var button = $(event.relatedTarget) // Botón que activó el modal
+      var idp = button.data('id') // Extraer la información de atributos de datos
+      var titulo = button.data('t') // Extraer la información de atributos de datos
+      var categoria = button.data('c') // Extraer la información de atributos de datos
+      
+      var modal = $(this)
+      modal.find('.modal-body #txtidpublic').val(idp)
+      modal.find('.modal-body #txttitulo2').val(titulo)
+      modal.find('.modal-body #txtcat2').val(categoria)
+      modal.find('.modal-body #txttitulo').val(titulo)
+      modal.find('.modal-body #txtcat').val(categoria)
+
+      $('.alert').hide();//Oculto alert
+
+
+})
+
+
+$('#btnAdjudicarCancelar').click(function() {  
+
+          $('#cboproveedor').val('');
+
+});
+
+$('#btnAdjudicarCancelarP').click(function() {  
+
+          $('#cboproveedorP').val('');
+          $("#mensajeAd").val('');
+
+});
+
+
